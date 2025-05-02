09b86529b1464089d5168fe2e4ecb93566be8aa8
Karla Tovar <karla@penteon.io>
2025-05-01 23:12:57 -0600

    Forzar deploy en dev

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ ruta, titulo, ocultarTitulo }) {
     <div className="w-full flex items-end justify-center relative mb-12" id="top">
       <img src={ruta} alt="imagen principal" className="w-[100%]" />
       {ocultarTitulo ? null : (
-        <div className="w-[75%] flex justify-center bg-blancoPrincipal rounded-xl absolute text-negroPrincipal -bottom-10" id="titulo_pagina">
+        <div className="w-[70%] flex justify-center bg-blancoPrincipal rounded-xl absolute text-negroPrincipal -bottom-10" id="titulo_pagina">
           <p className="font-medium text-3xl sm:text-4xl md:text-5xl lg:9xl p-5">{titulo}</p>
         </div>
       )}
